adb03c671b44119d911c50eaa60758cb9acbb144
Artem <chernysh.karina221@gmail.com>
2023-03-17 16:52:58 +0200

    "Frontend and beckend files"

---
+++ client/src/screens/ProductsScreen.jsx
@@ -0,0 +1,22 @@
+import {Center,Wrap,WrapItem} from '@chakra-ui/react';
+import { products } from '../products';
+import ProductCard from '../components/ProductCard';
+
+const ProductsScreen = () => {
+  return (
+    <Wrap spacing='30px' justify='center' minHeight='100vh'>
+      {products.map((product)=>{
+        return (
+            <WrapItem key={product._id}>
+              <Center w={250} h={550}>
+                <ProductCard product={product}/>
+              </Center>
+            </WrapItem>
+        )
+      })}
+    
+    </Wrap>
+  )
+}
+
+export default ProductsScreen
