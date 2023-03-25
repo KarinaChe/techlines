6f979851fe3f06604206236bc771e8a488049400
Artem <chernysh.karina221@gmail.com>
2023-03-25 21:38:31 +0200

    redux setup and products fetched from api

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,20 +1,39 @@
-import {Center,Wrap,WrapItem} from '@chakra-ui/react';
-import { products } from '../products';
+import {Center,Spinner,Wrap,WrapItem,Stack, Alert, AlertIcon, AlertTitle, AlertDescription} from '@chakra-ui/react';
+
 import ProductCard from '../components/ProductCard';
+import { useDispatch, useSelector } from 'react-redux';
+import {getProducts} from '../redux/actions/productActions';
+import { useEffect } from 'react';
 
 const ProductsScreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state)=>state.products);
+  const {loading,error,products} = productList;
+  useEffect(()=>{
+    dispatch(getProducts())
+  },[dispatch])
   return (
     <Wrap spacing='30px' justify='center' minHeight='100vh'>
-      {products.map((product)=>{
-        return (
-            <WrapItem key={product._id}>
-              <Center w={250} h={550}>
-                <ProductCard product={product}/>
-              </Center>
-            </WrapItem>
-        )
-      })}
-    
+      {loading ? (
+        <Stack direction='row' spacing={4}>
+          <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl'/>
+        </Stack>
+        ) : error ? (
+        <Alert status='error'>
+          <AlertIcon/>
+          <AlertTitle>Upps!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+        ) : (
+        products.map((product)=>(
+        
+          <WrapItem key={product._id}>
+            <Center w={250} h={550}>
+              <ProductCard product={product}/>
+            </Center>
+          </WrapItem>
+        
+      )))}
     </Wrap>
   )
 }
